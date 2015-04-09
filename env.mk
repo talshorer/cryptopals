65d924d7d1e043fa9346c766da6a0ff9d67dcc05
Tal Shorer <tal.shorer@gmail.com>
2015-04-09 20:06:36 +0300

    env.mk: compile .o files first
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- env.mk
+++ env.mk
@@ -1,18 +1,22 @@
-CFLAGS += -Wall -Werror -D_GNU_SOURCE -L.
+CFLAGS += -Wall -Werror -fPIC -D_GNU_SOURCE -L.
 
 CLEAN := rm -rf
 
 default: all
 
-%.out: %.c
+%.o: %.c
+	$(CC) -c $< -o $@ $(CFLAGS) $($@_CFLAGS)
+
+%.out: %.o
 	$(CC) $< -o $@ $(CFLAGS) $($@_CFLAGS)
 
-lib%.so: %.c
-	$(CC) $< -o $@ -shared -fPIC $(LMOD_US_CFLAGS) $($@_CFLAGS)
+lib%.so: %.o
+	$(CC) $< -o $@ -shared $(CFLAGS) $($@_CFLAGS)
 
 all: $(OBJ)
 
 clean: clean-obj clean-backup
+	$(CLEAN) *.o *.so *.out
 
 clean-obj:
 	$(CLEAN) $(OBJ)
